96d71940a33f0e5961205137969829526ed25b79
sis0k0 <stanimira.vlaeva@gmail.com>
2017-02-28 17:59:13 +0200

    feat: add webpack support

---
+++ app/vendor.js
@@ -0,0 +1,8 @@
+require("./vendor-platform");
+
+require("bundle-entry-points");
+
+require("./broadcastreceivers/NotificationEventReceiver.js")
+require("./broadcastreceivers/NotificationServiceStarterReceiver.js")
+require("./notifications/NotificationIntentService.js");
+
